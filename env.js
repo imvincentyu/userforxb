22a09cb09ef338dac16b1fe0322e70f76834418b
imvincentyu <134438846+imvincentyu@users.noreply.github.com>
2024-07-18 18:17:54 +0800

    Update and rename env.example.js to env.js

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://cinemacloud.shop'
+window.settings = {
+  // 站点名称
+  title: '電影院 FilmCloud',
+  // 站点描述
+  description: '“電影發明以後，人類的生命，比以前至少延長了3倍。”',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'black', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: 'https://image.stheadline.com/f/1500p0/0x0/100/none/6a00bd7feb2ef15476cc825f75188c02/stheadline/news_res/2021/05/13/424271/i_src_124132497.png',
+  // 站点LOGO
+  logo: 'https://3o.hk/images/2024/03/31/FilmCloudlogin.png',
+}
